1943b4db734da6c065a067ca4f75f7139095d31b
grisha <norsulfasol@gmail.com>
2022-10-31 21:35:11 +0300

    Объекты. Пятый коммит.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author strugatsky = new Author("Борис и Аркадий", "Стругатские");
+        Book mondayBeginsOnSaturday = new Book("Понедельник начинается в суббо" +
+                "ту", strugatsky, 1964);
+        Author marcusAurelius = new Author("Марк Аврелий", "Марк Элий Аврелий Вер Цезарь");
+        Book reasoningAboutYourself = new Book("Рассуждения о самом себ" +
+                "е.", marcusAurelius, 1914);
+        mondayBeginsOnSaturday.setYearOfPublication(1979);
+        System.out.println(reasoningAboutYourself.getNameBook() + " " + reasoningAboutYourself.getFullNameAuthor() +
+                " " + reasoningAboutYourself.getYearOfPublication());
+        System.out.println(mondayBeginsOnSaturday.getNameBook() + " " + mondayBeginsOnSaturday.getFullNameAuthor() +
+                " " + mondayBeginsOnSaturday.getYearOfPublication());
+
     }
+
 }
